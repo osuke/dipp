3cd353088a358d3018d9bfec348ef06b6fec94a7
OsukeUesugi <osk0712@i.softbank.jp>
2017-10-25 07:16:11 +0900

    added tabs to Result

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react'
 import styles from './result.css'
 import ImageDisplay from '../ImageDisplay/ImageDisplay'
+import {Tab, Tabs} from 'react-toolbox'
 
 export default class Result extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      index: 1,
+      fixedIndex: 1,
+      inverseIndex: 1
+    }
+  }
+
+  handleTabChange (index) {
+    this.setState({index})
+  }
   render () {
-    let src = this.props.file.diffImage
     return (
       <div className={styles.container}>
-        <ImageDisplay src={src} />
+        <Tabs className={styles.tabs} index={this.state.index} onChange={this.handleTabChange.bind(this)} fixed={true}>
+          <Tab label="Before">
+            <ImageDisplay src={this.props.file.beforeImage} />
+          </Tab>
+          <Tab label="After">
+            <ImageDisplay src={this.props.file.afterImage} />
+          </Tab>
+          <Tab label="Diff">
+            <ImageDisplay src={this.props.file.diffImage} />
+          </Tab>
+        </Tabs>
       </div>
     )
   }
