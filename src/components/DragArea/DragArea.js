8292cc97e02519ab5587e6138f5f11fcfbc4599b
OsukeUesugi <osk0712@i.softbank.jp>
2017-11-05 08:07:04 +0900

    Added drag over style

--- src/components/DragArea/DragArea.js
+++ src/components/DragArea/DragArea.js
@@ -2,12 +2,33 @@ import React, { Component } from 'react'
 import styles from './dragArea.css'
 
 export default class DragArea extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      eventName: ''
+    }
+  }
+
   componentDidUpdate () {
     if (!this.refs.canvas || this.props.file[this.props.cntxt].file) return
 
     this.ctx = this.refs.canvas.getContext('2d')
     this.ctx.drawImage(this.props.file[this.props.cntxt].obj, 0, 0, this.props.file[this.props.cntxt].obj.width, this.props.file[this.props.cntxt].obj.height, 0, 0, this.props.file[this.props.cntxt].width, this.props.file[this.props.cntxt].height)
     this.props.createPNGFile(this.props.cntxt, this.refs.canvas.toDataURL())
+  }
+
+  dragOverHandler (e) {
+    e.preventDefault()
+    this.setState(() => {
+      return { eventName: 'dragOver'}
+    })
+  }
+
+  dragLeaveHandler (e) {
+    e.preventDefault()
+    this.setState(() => {
+      return { eventName: 'dragLeave'}
+    })
   }
 
   render () {
@@ -26,13 +47,14 @@ export default class DragArea extends Component {
     } else {
       return (
         <div
-          className={styles.container}
-          onDragOver={(e) => { e.preventDefault() }}
+          className={`${styles.container} ${styles[this.state.eventName]}`}
+          onDragOver={this.dragOverHandler.bind(this)}
+          onDragLeave={this.dragLeaveHandler.bind(this)}
           onDrop={(e) => {
             e.preventDefault()
             this.props.extractFile(e, this.props.cntxt)
           }}>
-          {this.props.cntxt}
+          Drop an image
         </div>
       )
     }
