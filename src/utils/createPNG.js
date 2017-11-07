2991123e7efa14d6332acb12d08d2e6e201304e5
OsukeUesugi <osk0712@i.softbank.jp>
2017-11-07 22:48:30 +0900

    remove unnecessary code

--- src/utils/createPNG.js
+++ src/utils/createPNG.js
@@ -1,33 +1,15 @@
 import fs from 'fs'
-import Jimp from 'jimp'
 
 export default (fileName, base64) => (
   new Promise((resolve, reject) => {
     const arr = base64.split(';base64,')
 
-    if (arr[0].indexOf('jpeg') !== -1) {
-      fs.writeFile(fileName + '.jpg', arr[1], {encoding: 'base64'}, (err) => {
-        if (err) {
-          return
-        }
-
-        Jimp.read(fileName + '.jpg', (err, lenna) => {
-          if (err) {
-            return
-          }
-
-          lenna.quality(100).write(fileName + '.png')
-          resolve('File created')
-        })
-      })
-    } else {
-      fs.writeFile('./tmp/' + fileName + '.png', arr[1], {encoding: 'base64'}, (err) => {
-        if (err) {
-          return
-        }
-
-        resolve('File created')
-      })
-    }
+    fs.writeFile('./tmp/' + fileName + '.png', arr[1], {encoding: 'base64'}, (err) => {
+      if (err) {
+        return
+      }
+    
+      resolve('File created')
+    })
   })
 )
